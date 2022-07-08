295bfaac46830ec880c3faba523528ca09658a24
Gouthami8616 <107345552+Gouthami8616@users.noreply.github.com>
2022-07-08 17:11:24 +0530

    Update text.py

--- text.py
+++ text.py
@@ -2,3 +2,5 @@ from app import Calculator
 
 cal = Calculator(a=3, b=4)
 print(f"Adding te values :- {cal.addition()}")
+print(f"subraction the values :- {cal.subraction()}")
+print(f"multiplication the values :- {cal.multiplication()}")
